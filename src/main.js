f11c9114d2e44eecd24f5d27f3a21d89174ed604
iigmir <roc120j@gmail.com>
2020-08-28 03:19:02 +0800

    Linting

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// Fort Awesome
+library.add( fas );
+Vue.component( "vue-fontawesome", FontAwesomeIcon );
+Vue.use(Buefy, {
+    defaultIconComponent: "vue-fontawesome",
+    defaultIconPack: "fas",
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
