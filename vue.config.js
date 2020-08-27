9d68d6ac55a74f4f6c509624831333b433cc6c06
iigmir <roc120j@gmail.com>
2020-08-28 03:44:59 +0800

    XML loader set

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,15 @@
 module.exports = {
-    // module: {
-    //     rules: [{
-    //         test: /\.xml$/,
-    //         use: {
-    //             loader: "xml-loader",
-    //             options: {
-    //                 explicitArray: false,
-    //             },
-    //         },
-    //     }]
-    // }
-    chainWebpack: config => {
-        // GraphQL Loader
-        config.module
-            .rule("xml")
-            .test(/\.xml$/)
-            .use("xml-loader")
-            .loader("xml-loader")
-            // .tap(() => [{
-            //     explicitArray: false,
-            // }])
-            .end();
+    configureWebpack: {
+        module: {
+            rules: [{
+                test: /\.xml$/,
+                use: {
+                    loader: "xml-loader",
+                    options: {
+                        explicitArray: false,
+                    },
+                },
+            }]
+        }
     }
 };
