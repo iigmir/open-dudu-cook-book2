45fe9091c515b08314c80632dfe0938d78a3e619
iigmir <roc120j@gmail.com>
2020-08-29 01:41:27 +0800

    Delete debug_info

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,6 @@
 <script>
 export default {
     name: "open-dudu2",
-    computed: {
-        debug_info() {
-            return {
-                electron: process.versions.electron,
-                node: process.versions.node,
-                platform: require("os").platform(),
-                vue: require("vue/package.json").version
-            };
-        }
-    }
 };
 </script>
 
