6901ee5b24ba5420472c3c35ab98c01ab8881b13
iigmir <roc120j@gmail.com>
2020-08-29 01:42:38 +0800

    Set vuex apis

--- src/store/modules/RecipeFlag.js
+++ src/store/modules/RecipeFlag.js
@@ -1,7 +1,16 @@
 const state = {
     sorted_type: "",
     sorted_tag: "",
-    sorted_id: ""
+    sorted_id: "",
+    show_Menu: true,
+    show_RecipesList: true
+};
+const mutset = {
+    sorted_type: "set_sorted_type",
+    sorted_tag: "set_sorted_tag",
+    sorted_id: "set_sorted_id",
+    show_Menu: "set_show_Menu",
+    show_RecipesList: "set_show_RecipesList"
 };
 
 const getters = {
@@ -13,30 +22,48 @@ const getters = {
     },
     sorted_id_getter(state) {
         return state.sorted_id;
-    }
+    },
+    shown_flag_getter(state) {
+        return {
+            show_Menu: state.show_Menu,
+            show_RecipesList: state.show_RecipesList,
+        };
+    },
 };
 
 const mutations = {
-    set_sorted_type(state, value = "") {
+    [mutset.sorted_type](state, value = "") {
         state.sorted_type = value;
     },
-    set_sorted_tag(state, value = "") {
+    [mutset.sorted_tag](state, value = "") {
         state.sorted_tag = value;
     },
-    set_sorted_id(state, value = "") {
+    [mutset.sorted_id](state, value = "") {
         state.sorted_id = value;
+    },
+    [mutset.show_Menu](state, value = true) {
+        state.show_Menu = value;
+    },
+    [mutset.show_RecipesList](state, value = true) {
+        state.show_RecipesList = value;
     },
 };
 
 const actions = {
     SET_sorted_type({ commit }, value = "") {
-        commit( "set_sorted_type", value );
+        commit( mutset.sorted_type, value );
     },
     SET_sorted_tag({ commit }, value = "") {
-        commit( "set_sorted_tag", value );
+        commit( mutset.sorted_tag, value );
     },
     SET_sorted_id({ commit }, value = "") {
-        commit( "set_sorted_id", value );
+        commit( mutset.sorted_id, value );
+    },
+    SET_toggle_show_Menu({ commit, state }) {
+        commit( mutset.show_Menu, !state.show_Menu );
+    },
+    SET_toggle_show_RecipesList({ commit, state }) {
+        commit( mutset.show_RecipesList, !state.show_RecipesList );
     },
 };
 
